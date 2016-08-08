aec665790afe7b4235481a24c5296fd362415b29
Austin Peterson <austin@akpwebdesign.com>
2016-08-07 20:32:03 -0500

    Initial language list working.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,57 @@
 var languages = {en: "No."};
 var s = new Storage();
-var hbs, hbt;
+var answerTemplate, buttonTemplate;
 
 $(document).ready(function(){
-	hbs = $("#answer-template").html();
-	hbt = Handlebars.compile(hbs);
+	answerTemplate = Handlebars.compile($("#answer-template").html());
+	buttonTemplate = Handlebars.compile($("#lang-dropdown-button-template").html());
 	$.getJSON( "lang/all.json", function( data ) {
 	  languages = data;
-		changeLang(s.get('lang-code') || 'en');
+		changeLang(s.get('lang-code') || 'en_US');
+		prepareLangPanel(data);
 	});
 });
 
-$('.languages li a').click(function() {
-	var lang = $(this).data('lang');
-	s.set('lang-code', changeLang(lang));
+$('.langButton, .langCloseButton').click(function(){
+	$('.languages-panel').toggleClass('onscreen');
+	$('.languages-panel').toggleClass('offscreen');
 });
 
 function changeLang(lang) {
-	if(!languages[lang]) { console.log('Unsupported language code '+lang); lang = 'en'; }
-	console.log('Language = ' + lang);
-	var d = {answer: languages[lang]};
-	console.log('Data = ', d);
-	$("#answer").html(hbt(d));
+	if(!languages[lang]) { console.log('Unsupported language code '+lang); lang = 'en_us'; }
+	$("#answer").html(answerTemplate({answer: languages[lang].answer}));
+	$("#langDropdown").html(buttonTemplate({name: languages[lang].name}));
 	return lang;
+}
+
+function prepareLangPanel(data) {
+	var temp = $("#lang-dropdown-list-item-template").html();
+	var compiled = Handlebars.compile(temp);
+	var byNames = {}, key, keys = [];
+
+	for (key in data) {
+		if (data.hasOwnProperty(key)) {
+			byNames[data[key].name] = key;
+		}
+	}
+
+	for (key in byNames) {
+		keys.push(key);
+	}
+
+	keys.sort();
+
+	for (var i = 0; i < keys.length; i++) {
+		var d = byNames[keys[i]];
+		$(".languages").append(
+			compiled({lang: d, name: keys[i]})
+		);
+	}
+
+	$('.languages li a').click(function() {
+		var lang = $(this).data('lang');
+		s.set('lang-code', changeLang(lang));
+		$('.languages-panel').toggleClass('onscreen');
+		$('.languages-panel').toggleClass('offscreen');
+	});
 }
